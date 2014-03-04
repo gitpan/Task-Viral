1f9f5f3d06bb45fc90ad7968f3472a01517e9dbd
Gianluca Casati <fibo@cpan.org>
2014-03-04 11:56:50 +0000

    Import of FIBO/Task-Viral-20140304 from CPAN.
    gitpan-cpan-distribution: Task-Viral
    gitpan-cpan-version:      20140304
    gitpan-cpan-path:         FIBO/Task-Viral-20140304.tar.gz
    gitpan-cpan-author:       FIBO
    gitpan-cpan-maturity:     released

--- lib/Task/Viral.pm
+++ lib/Task/Viral.pm
@@ -1,12 +1,23 @@
 use strict;
 use warnings;
 package Task::Viral;
-{
-  $Task::Viral::VERSION = '20130508';
-}
 # ABSTRACT: Conquering hosts
-
+$Task::Viral::VERSION = '20140304';
 1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Task::Viral - Conquering hosts
+
+=head1 VERSION
+
+version 20140304
 
 =head1 DESCRIPTION
 
@@ -16,7 +27,6 @@ Suppose you have the following topology.
 
     Internet <- host1 <--> host2
 
-
 =head1 STUFF INCLUDED
 
 =item Task::Pinto
@@ -25,5 +35,15 @@ Suppose you have the following topology.
 
 =item App::perlbrew
 
-=cut
+=head1 AUTHOR
 
+G. Casati <fibo@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by G. Casati.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
